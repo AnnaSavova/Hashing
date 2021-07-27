f774c4f5ab5b3171fa70a7e408d805039acab56f
Atanas Semerdzhiev <semerdjiev@live.com>
2021-07-27 16:06:52 +0300

    Code Review

---
+++ src/MyHash.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class MyHash {
+    
+    List<Integer> [] buckets;
+    int elementsCount = 0;
+
+    public MyHash(int bucketsCount) {
+        buckets = new LinkedList[bucketsCount];
+
+        for(int i = 0; i < buckets.length; ++i) {
+            buckets[i] = new LinkedList<Integer>();
+        }
+
+    }
+
+    private int hash(Integer value) {
+        return value % buckets.length;
+    }
+
+    public void add(Integer value) {
+        int h = hash(value);
+
+        if(! contains(value)) {
+            buckets[h].add(value);
+            ++elementsCount;
+        }
+    }
+
+    public boolean contains(Integer value) {
+        int h = hash(value);
+        return buckets[h].contains(value);
+    }
+
+    public void remove(Integer value) {
+        int h = hash(value);
+
+        if(contains(value)) {
+            buckets[h].remove(value);
+            --elementsCount;
+        }        
+    }
+
+    public int size() {
+        return elementsCount;
+    }
+
+    public int loadFactor() {
+        int max = 0;
+        for(int i = 0; i < buckets.length; ++i) {
+            if(buckets[i].size() > max) {
+                max = buckets[i].size();
+            }
+        }
+        return max;
+    }
+}
--- test/HashingTest.java
+++ test/HashingTest.java
@@ -4,16 +4,49 @@ import org.junit.jupiter.api.Test;
 import java.util.Objects;
 
 class HashingTest {
-    public Hashing h1 = new Hashing("abcd");
+    final int elementsCount = 100;
+    final int bucketsCount = elementsCount / 10;
+    public MyHash h1 = new MyHash(bucketsCount);
 
-    @Test
-    public void indexInitializesCorrectly(){
-        assert h1.key == 0;
-        assert h1.getSize() == 1;
-    }
-    @Test
-    public void get_returnsNonNull() throws Exception {
-        assert Objects.isNull(h1.get(0));
+    public HashingTest() {
+        for(int i = 0; i < elemensCount; ++i) {
+            h1.add(i);
+        }
     }
 
+    @Test
+    public void size_returnsCorrectValue() {
+        assert h1.size() == elementsCount;
+    }
+
+    @Test
+    public void contains_returnsTrueForAllAddedElements() {
+        for(int i = 0; i < elementsCount; ++i) {
+            assert h1.contains(i);
+        }
+    }
+
+    @Test
+    public void contains_returnsFalseForAllElementsWhichWereNotAdded() {
+        for(int i = elementsCount; i < elementsCount + 100; ++i) {
+            assert ! h1.contains(i);
+        }
+
+        for(int i = -100; i < 0; ++i) {
+            assert ! h1.contains(i);
+        }        
+    }
+
+    @Test
+    public void remove_removesCorrectly() {
+        for(int i = 0; i < elementsCount; ++i) {
+            h1.remove(i);
+            assert ! h1.contains(i);
+        }
+    }
+
+    @Test
+    public loadFactor_returnsCorrectValue() {
+        assert h1.loadFactor() == bucketsCount;
+    }
 }
